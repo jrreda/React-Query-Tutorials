d22e1e8020f3bc9db74fba445b105c9ef07b6ed2
Reda <mahmoudreda457@gmail.com>
2026-01-29 13:28:58 +0200

    Errors

--- src/components/SuperHeroes.page.jsx
+++ src/components/SuperHeroes.page.jsx
@@ -4,16 +4,24 @@ import axios from "axios";
 export default function SuperHeroesPage() {
   const [superHeroes, setSuperHeroes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     axios.get("http://localhost:4000/superheroes").then((response) => {
       setSuperHeroes(response.data);
+    }).catch((error) => {
+      setError(error);
+    }).finally(() => {
       setIsLoading(false);
     });
   }, []);
 
   if (isLoading) {
     return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
   }
 
   return (
