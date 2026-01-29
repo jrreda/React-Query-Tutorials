6f27af95b992c90c89dd4f0ce792a08c090fbaca
Reda <mahmoudreda457@gmail.com>
2026-01-29 13:26:33 +0200

    Fetching Data with useQuery

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -1,3 +1,32 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = async () => {
+  return await axios.get("http://localhost:4000/superheroes");
+};
+
 export default function RQSuperHeroesPage() {
-  return <div>React Query - Super Heroes Page</div>;
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["super-heroes"],
+    queryFn: fetchSuperHeroes,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h2>React Query - Super Heroes Page</h2>
+      <ul>
+        {data?.data.map((hero) => (
+          <li key={hero.id}>{hero.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
