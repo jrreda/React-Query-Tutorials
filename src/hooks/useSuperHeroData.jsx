fc1ea24b96216bb76cac243c80bf052f0304b319
Reda <mahmoudreda457@gmail.com>
2026-01-30 13:29:57 +0200

    Initial Query Data

--- src/hooks/useSuperHeroData.jsx
+++ src/hooks/useSuperHeroData.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query'
+import { useQuery, useQueryClient } from 'react-query'
 import axios from 'axios'
 
 const fetchSuperHero = async ({ queryKey }) => {
@@ -7,9 +7,19 @@ const fetchSuperHero = async ({ queryKey }) => {
 }
 
 export default function useSuperHeroData({ heroId }) {
+  const queryClient = useQueryClient();
   const { data, isLoading, error, isError, isFetching, refetch } = useQuery({
     queryKey: ["super-hero", heroId],
     queryFn: fetchSuperHero,
+    initialData: () => {
+      const hero = queryClient.getQueryData("super-heroes")?.data?.find((hero) => hero.id === parseInt(heroId));
+
+      if (hero) {
+        return { data: hero };
+      } else {
+        return undefined;
+      }
+    }
   });
 
   return { data, isLoading, error, isError, isFetching, refetch }
