ddf9d8dc05eb633bf1ecb7036e50e025232aefa9
Reda <mahmoudreda457@gmail.com>
2026-01-31 18:30:30 +0200

    Optimistic Updates

--- src/hooks/useSuperHeroesData.jsx
+++ src/hooks/useSuperHeroesData.jsx
@@ -30,14 +30,31 @@ export default function useSuperHeroesData(onSuccess, onError, interval) {
 export function useAddSuperHeroData() {
   const queryClient = useQueryClient();
   return useMutation(addSuperHero, {
-    onSuccess: (data) => {
-      // queryClient.invalidateQueries("super-heroes"); // unnecessary network request
+    // onSuccess: (data) => {
+    //   // queryClient.invalidateQueries("super-heroes"); // unnecessary network request
+    //   queryClient.setQueryData("super-heroes", (oldData) => {
+    //     return {
+    //       ...oldData,
+    //       data: [...oldData.data, data.data]
+    //     }
+    //   })
+    // }
+    onMutate: (newHero) => {
+      // optimistically update the data
       queryClient.setQueryData("super-heroes", (oldData) => {
         return {
           ...oldData,
-          data: [...oldData.data, data.data]
+          data: [...oldData.data, { ...newHero, id: oldData.data.length + 1 }]
         }
       })
+    },
+    onError: (_error, _newHero, context) => {
+      // rollback to the previous data
+      queryClient.setQueryData("super-heroes", context.previousData)
+    },
+    onSettled: () => {
+      // invalidate the query to get the latest data
+      queryClient.invalidateQueries("super-heroes")
     }
   })
 }
